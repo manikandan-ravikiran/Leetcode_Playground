698a68589b583061b7f953d191316067299696a4
Manikandan <vishnumani.2009@rediffmail.com>
2020-04-09 18:23:59 +0530

    Backstring compare

---
+++ arrays/backstringcompare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        import itertools
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+
+        return all(x == y for x, y in itertools.zip_longest(F(S), F(T)))
+        
+        
